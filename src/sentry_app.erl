ee80e5d59affa5cd0c0cc61b82e71f32bb823725
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2018-04-13 15:46:20 +0200

    Commit the delay logic

--- src/sentry_app.erl
+++ src/sentry_app.erl
@@ -1,26 +1,18 @@
-%%%-------------------------------------------------------------------
-%% @doc sentry public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(sentry_app).
 
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
-
-%%====================================================================
-%% API
-%%====================================================================
+-export([start/2, stop/1, prep_stop/1]).
 
 start(_StartType, _StartArgs) ->
     sentry_sup:start_link().
 
-%%--------------------------------------------------------------------
 stop(_State) ->
     ok.
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+prep_stop(State) ->
+    Delay = timer:seconds(5),
+    error_logger:error_msg("sentry prep-stop: delay ~pms\n", [Delay]),
+    timer:sleep(Delay),
+    State.
--- src/sentry_sup.erl
+++ src/sentry_sup.erl
@@ -28,7 +28,14 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+    {ok, { {one_for_all, 0, 1}, [
+                                 #{id => sentry_worker,
+                                   start => {sentry_worker, start_link, []},
+                                   restart => permanent,
+                                   shutdown => 5000,
+                                   type => worker,
+                                   modules => [sentry_worker]}
+                                ]} }.
 
 %%====================================================================
 %% Internal functions
